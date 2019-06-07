c7ae3b84da39427cb9fd852b21b7e0045b18376e
ting121777 <ting121777@gmail.com>
2019-06-07 14:19:32 +0800

    喪屍start, default分類

--- AutoScriptVisualTool/Form1.cs
+++ AutoScriptVisualTool/Form1.cs
@@ -1084,10 +1084,339 @@ namespace AutoScriptVisualTool
                     }
                     ++k2;
                 }
+                /////////////////////////////////////////////////////
             }
             else if (script_num == 3)
             {
+                /////////////////////////////////////////
+                ListBox list = new ListBox();
+                string[] items =
+                {
+                    "start0", "start1"
+                };
+                Dictionary<int, string[]> dic = new Dictionary<int, string[]>
+                {
+                    {0, new string[]{
+                        "SceneStart"
+                    }},
+                    {1, new string[]{
+                        "SceneStart", "DontMove", "CanMove", "KEsc"
+                    }}
+                };
+                Dictionary<int, string[]> dic2 = new Dictionary<int, string[]>
+                {
+                    {0, new string[]{
+                        "true # ChangeText Caption 末日求生 reserve 255,255,0 0,120,B -1",
+                        "true # ChangeText Start/Text 開始新遊戲 reserve 255,0,0 reserve -1",
+                        "true # ChangeText Load/Text 繼續遊戲 reserve 0,0,255 reserve -1",
+                        "true # ChangeText Quit/Text 離開遊戲 reserve 0,255,0 reserve -1",
+                        "true # ChangeImage Back zombie.jfif reserve 255,255,255,1 -1",
+                        "true # Virtual Slider at 0,0,0",
+                        "true # ChBGMfrom Kao"
+                    }},
+                    {1, new string[]{
+                        "true # SetVar public 99 0",
+                        "true # ShowText ??? 1700,1000 0,255,0 1,48,B -1 as Place",
+                        "true # ChBGMfrom Kao",
+                        "true # Virtual Default at 0,0,0",
+                        "true # Virtual DontMove at 0,0,0",
+                        "true # Virtual CanMove at 0,0,0",
+                        "true # NPC Blackman in role 0,-10,10 as Zombie1",
+                        "true # NPC Zombie in role 10,-10,0 as Zombie2",
+                        "true # NPC Remy in role 0,-10,20 as Zombie3",
+                        "true # NPC Azure in role 20,-10,-10 as Zombie4",
+                        "true # NPC Mei in role 10,-10,10 as Zombie5",
+                        "true # NPC White in role 5,-10,20 as Zombie6",
+                        "true # Route Zombie1 0,0,0 10,0,0",
+                        "true # Route Zombie2 0,0,0 0,0,20",
+                        "true # Route Zombie3 0,0,0 5,0,0",
+                        "true # Route Zombie5 0,0,0 0,0,-10",
+                        "true # Route Zombie6 0,0,0 5,0,3",
+                        "true # Create Rock in role 10,-10,10 as Rock",
+                        "true # Create Rock2 in role 10,-10,20 as Rock2",
+                        "true # Create Tree in role 10,-10,5 as Tree",
+                        "true # Create Chest in role 15,-10,5 as Chest",
+                        "true # Create Brickwall in role 20,-10,5 as Brickwall",
+                        "true # Bind down Esc KEsc +"
+                    }},
+                    {2, new string[]{
+                        "true # Bind press W null",
+                        "true # Bind press S null",
+                        "true # Bind press A null",
+                        "true # Bind press D null",
+                        "true # Bind up W null",
+                        "true # Bind up S null"
+                    }},
+                    {3, new string[]{
+                        "true # Bind press W myWalk",
+                        "true # Bind press S myBack",
+                        "true # Bind press A myLeft",
+                        "true # Bind press D myRight",
+                        "true # Bind up W stopWalk",
+                        "true # Bind up S stopWalk"
+                    }},
+                    {4, new string[]
+                    {
+                        "== g99 5 # Goto event1",
+                        "true # Menu",
+                        "true # Return",
+                        "event1:",
+                        "true # DefineStr 5 $str0",
+                        "true # ChangeText Msg $5 900,900 0,255,255 1,48,B -1",
+                        "true # Return"
+                    }}
+                };
 
+                list.Items.AddRange(items);
+                start_list.Items.Clear();
+                start_list.Items.AddRange(list.Items);
+
+                int k = 0, q = 0, num = 1; // start=1, trigger 2, destroy 3, update 4, player 5, default 7
+
+                foreach (object i in start_list.Items)
+                {
+                    map.Add(i, new Script_form(num));
+                    Script_form sub = (Script_form)map[i];
+                    foreach (string s in dic[k])
+                    {
+                        sub.class_list.Items.Add(s);
+                        sub.class_list_ItemAdded(num);
+                        sub.get_sub_form().event_list.Items.AddRange(dic2[q]);
+                        ++q;
+                    }
+                    ++k;
+                }
+                /////////////////////////////////////////
+                ListBox list1 = new ListBox();
+                string[] items1 =
+                {
+                    "default"
+                };
+                Dictionary<int, string[]> dic3 = new Dictionary<int, string[]>
+                {
+                    {0, new string[]{
+                        "Default",
+                        "KeyDownH",
+                        "KeyPressH",
+                        "KeyUpH",
+                        "KTab",
+                        "Skill0",
+                        "XS",
+                        "CS",
+                        "VS",
+                        "myWalk",
+                        "myBack",
+                        "myLeft",
+                        "myRight",
+                        "myJump",
+                        "stopWalk",
+                        "SpeedUp",
+                        "SpeedDown"
+                    }}
+                };
+                Dictionary<int, string[]> dic4 = new Dictionary<int, string[]>
+                {
+                    {0, new string[]{
+                        "true # Broadcast 使用預設檔案",
+                        "true # Bind down H KeyDownH +",
+                        "true # Bind press H KeyPressH +",
+                        "true # Bind up H KeyUpH +",
+                        "true # Bind down Tab KTab +",
+                        "true # Bind down Z Skill0 +",
+                        "true # Bind down X XS +",
+                        "true # Bind down C CS +",
+                        "true # Bind down V VS +",
+                        "true # Bind down O OS +",
+                        "true # Bind down Shift SpeedUp +",
+                        "true # Bind up Shift SpeedDown +",
+                        "true # Bind press W myWalk +",
+                        "true # Bind press S myBack +",
+                        "true # Bind press A myLeft +",
+                        "true # Bind press D myRight +",
+                        "true # Bind down A GasA +",
+                        "true # Bind down B GasB +",
+                        "true # Bind down C GasC +",
+                        "true # Bind down D GasD +",
+                        "true # Bind up W stopWalk +",
+                        "true # Bind up S stopWalk",
+                        "true # Bind down Space myJump +"
+                    }},
+                    {1, new string[]{
+                        "true # Eval 0 to g2"
+                    }},
+                    {2, new string[]{
+                        "true # Eval 1 + g2 to g2"
+                    }},
+                    {3, new string[]{
+                        "true # Cast g2"
+                    }},
+                    {4, new string[]{
+                        "true # NextOne"
+                    }},
+                    {5, new string[]{
+                        "true # UseSkill 0"
+                    }},
+                    {6, new string[]{
+                        "true # UseSkill 1"
+                    }},
+                    {7, new string[]{
+                        "true # UseSkill 2"
+                    }},
+                    {8, new string[]{
+                        "true # UseSkill 3"
+                    }},
+                    {9, new string[]{
+                        "true # WalkForward"
+                    }},
+                    {10, new string[]{
+                        "true # WalkBackward"
+                    }},
+                    {11, new string[]{
+                        "true # WalkLeft"
+                    }},
+                    {12, new string[]{
+                        "true # WalkRight"
+                    }},
+                    {13, new string[]{
+                        "== l1 1 # Return",
+                        "true # StopWalk",
+                        "true # AddForce role 0,40000,0",
+                        "true # SetVar private 1 1",
+                        "true # Pause 1",
+                        "true # SetVar private 1 0"
+                    }},
+                    {14, new string[]{
+                        "true # StopWalk"
+                    }},
+                    {15, new string[]{
+                        "true # Broadcast 加速！",
+                        "true # GetSp l2 $speed 4",
+                        "true # Eval + l2 * 2 to l2",
+                        "< l2 15 # SetSp 4 $speed l2"
+                    }},
+                    {16, new string[]{
+                        "true # GetSp l2 $speed 4",
+                        "true # Eval + l2 / 2 to l2",
+                        "true # Cast l2",
+                        "true # SetSp 4 $speed l2"
+                    }}
+                };
+
+                list1.Items.AddRange(items1);
+                default_list.Items.Clear();
+                default_list.Items.AddRange(list1.Items);
+
+                int k1 = 0, q1 = 0, num1 = 7; // start=1, trigger 2, destroy 3, update 4, player 5, default 7
+
+                foreach (object i in default_list.Items)
+                {
+                    map.Add(i, new Script_form(num1));
+                    Script_form sub = (Script_form)map[i];
+                    foreach (string s in dic3[k1])
+                    {
+                        sub.class_list.Items.Add(s);
+                        sub.class_list_ItemAdded(num1);
+                        sub.get_sub_form().event_list.Items.AddRange(dic4[q1]);
+                        ++q1;
+                    }
+                    ++k1;
+                }
+                /////////////////////////////////////////
+                ListBox list2 = new ListBox();
+                string[] items2 =
+                {
+                    "trigger1", "trigger2","trigger10"
+                };
+                Dictionary<int, string[]> dic5 = new Dictionary<int, string[]>
+                {
+                    {0, new string[]{
+                        "Door", "NPC4"
+                    }},
+                    {1, new string[]{
+                        "Monster", "Princess"
+                    }},
+                    {2, new string[]{
+                        "King", "Statue"
+                    }}
+                };
+                Dictionary<int, string[]> dic6 = new Dictionary<int, string[]>
+                {
+                    {0, new string[]{
+                        "true # Broadcast 傳送",
+                        "true # LoadScene 10"
+                    }},
+                    {1, new string[]{
+                        "true # PushTalk 喔!天啊!看天上那是什麼？ 村民 $7",
+                        "true # PushTalk 看起來像是一條龍呢 勇者 $10",
+                        "true # PushTalk 他的爪子是不是還抓著什麼? 村民 $7",
+                        "true # PushTalk 嗯...看起來像是個女人... 勇者 $10",
+                        "true # PushTalk 注意!注意! 傳令兵 $6",
+                        "true # PushTalk 惡龍昆圖庫塔卡提考特蘇瓦西拉松抓走了公主米婭莫拉蘇娜丹妮謝莉红! 傳令兵 $6",
+                        "true # PushTalk 國王急徵勇者前往營救! 傳令兵 $6",
+                        "true # PushTalk 找到傳送門前往王都報到吧! 傳令兵 $6",
+                        "true # PushTalk 太好了，輪到我達拉崩巴斑得貝迪卜多比鲁翁出場了! 勇者 $10",
+                        "true # StartTalk"
+                    }},
+                    {2, new string[]{
+                        "true # PushTalk 我是昆圖庫塔卡提考特蘇瓦西拉松 惡龍 dragon.jpg",
+                        "true # PushTalk 再來一次 惡龍 dragon.jpg",
+                        "true # PushTalk 昆圖庫塔卡提考特蘇瓦西拉松 惡龍 dragon.jpg",
+                        "true # PushTalk 是不是昆特牌提琴烤蛋達蘇打馬拉松 勇者 $10",
+                        "true # PushTalk 不對，是昆圖庫塔卡提考特蘇瓦西拉松 惡龍 dragon.jpg",
+                        "true # PushTalk 於是，達拉崩巴斑得貝迪卜多比鲁翁，砍向 旁白 White.png",
+                        "true # PushTalk 昆圖庫塔卡提考特蘇瓦西拉松 旁白 White.png",
+                        "true # PushTalk 然後，昆圖庫塔卡提考特蘇瓦西拉松，咬了 旁白 White.png",
+                        "true # PushTalk 達拉崩巴斑得貝迪卜多比鲁翁 旁白 White.png",
+                        "true # PushTalk 最後，達拉崩巴斑得貝迪卜多比鲁翁，他戰勝了 旁白 White.png",
+                        "true # PushTalk 昆圖庫塔卡提考特蘇瓦西拉松 旁白 White.png",
+                        "true # PushTalk 救出了，公主米婭莫拉蘇娜丹妮謝莉红 旁白 White.png",
+                        "true # StartTalk",
+                        "true # Destroy 0"
+                    }},
+                    {3, new string[]{
+                        "true # PushTalk 我是公主米婭莫拉蘇娜丹妮謝莉红 公主 princess.jpg",
+                        "true # PushTalk 安安你好給虧嗎 公主 princess.jpg",
+                        "true # StartTalk"
+                    }},
+                    {4, new string[]{
+                        "true # PushTalk 我要帶上最好的劍 勇者 $10",
+                        "true # PushTalk 翻過最高的山 勇者 $10",
+                        "true # PushTalk 闖進最深的森林 勇者 $10",
+                        "true # PushTalk 把公主帶回到面前 勇者 $10",
+                        "true # PushTalk 朕非常高興，汝姓名為何 國王 king.jpg",
+                        "true # PushTalk 陛下我叫達拉崩巴班得貝迪卜多比魯翁 勇者 $10",
+                        "true # PushTalk 是不是達拉崩巴班得貝迪卜多比魯翁 國王 king.jpg",
+                        "true # PushTalk 對對達拉崩巴班得貝迪卜多比魯翁 勇者 $10",
+                        "true # PushTalk 本來按照歌詞你要慢慢打怪升級 國王 king.jpg",
+                        "true # PushTalk 可是demo時間有限，你直接走傳送門吧-> 國王 king.jpg",
+                        "true # StartTalk"
+                    }},
+                    {5, new string[]{
+                        "true # Broadcast 傳送",
+                        "true # LoadScene 2"
+                    }}
+                };
+
+                list2.Items.AddRange(items2);
+                trigger_list.Items.Clear();
+                trigger_list.Items.AddRange(list2.Items);
+
+                int k2 = 0, q2 = 0, num2 = 2; // start=1, trigger 2, destroy 3, update 4, player 5, default 7
+
+                foreach (object i in trigger_list.Items)
+                {
+                    map.Add(i, new Script_form(num2));
+                    Script_form sub = (Script_form)map[i];
+                    foreach (string s in dic5[k2])
+                    {
+                        sub.class_list.Items.Add(s);
+                        sub.class_list_ItemAdded(num2);
+                        sub.get_sub_form().event_list.Items.AddRange(dic6[q2]);
+                        ++q2;
+                    }
+                    ++k2;
+                }
+                /////////////////////////////////////////////////////
             }
             else if (script_num == 4)
             {
