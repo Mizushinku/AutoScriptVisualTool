6f39e2f7bad2b5ba0443090f7f756b69f10195f1
YJKuo <hinet0817@gmail.com>
2019-06-11 20:24:39 +0800

    Change File Output Method

--- AutoScriptVisualTool/Setting_Form.Designer.cs
+++ AutoScriptVisualTool/Setting_Form.Designer.cs
@@ -30,15 +30,14 @@
         {
             this.add_act_btn = new System.Windows.Forms.Button();
             this.event_list = new System.Windows.Forms.ListBox();
-            this.outfile_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // add_act_btn
             // 
-            this.add_act_btn.Location = new System.Drawing.Point(20, 42);
-            this.add_act_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.add_act_btn.Location = new System.Drawing.Point(27, 52);
+            this.add_act_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.add_act_btn.Name = "add_act_btn";
-            this.add_act_btn.Size = new System.Drawing.Size(77, 53);
+            this.add_act_btn.Size = new System.Drawing.Size(103, 66);
             this.add_act_btn.TabIndex = 0;
             this.add_act_btn.Text = "Add\r\nAction";
             this.add_act_btn.UseVisualStyleBackColor = true;
@@ -48,34 +47,24 @@
             // 
             this.event_list.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.event_list.FormattingEnabled = true;
-            this.event_list.ItemHeight = 16;
-            this.event_list.Location = new System.Drawing.Point(116, 42);
-            this.event_list.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.event_list.ItemHeight = 20;
+            this.event_list.Location = new System.Drawing.Point(155, 52);
+            this.event_list.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.event_list.Name = "event_list";
-            this.event_list.Size = new System.Drawing.Size(644, 468);
+            this.event_list.Size = new System.Drawing.Size(857, 584);
             this.event_list.TabIndex = 1;
-            // 
-            // outfile_btn
-            // 
-            this.outfile_btn.Location = new System.Drawing.Point(20, 118);
-            this.outfile_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.outfile_btn.Name = "outfile_btn";
-            this.outfile_btn.Size = new System.Drawing.Size(77, 53);
-            this.outfile_btn.TabIndex = 2;
-            this.outfile_btn.Text = "Output\r\nFile";
-            this.outfile_btn.UseVisualStyleBackColor = true;
-            this.outfile_btn.Click += new System.EventHandler(this.outfile_btn_Click);
+            this.event_list.KeyDown += new System.Windows.Forms.KeyEventHandler(this.event_list_KeyDown);
+            this.event_list.MouseDown += new System.Windows.Forms.MouseEventHandler(this.event_list_MouseDown);
             // 
             // Setting_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(906, 530);
-            this.Controls.Add(this.outfile_btn);
+            this.ClientSize = new System.Drawing.Size(1208, 662);
             this.Controls.Add(this.event_list);
             this.Controls.Add(this.add_act_btn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Setting_Form";
             this.Text = "Setting_Form";
             this.ResumeLayout(false);
@@ -85,7 +74,6 @@
         #endregion
 
         private System.Windows.Forms.Button add_act_btn;
-        private System.Windows.Forms.Button outfile_btn;
         internal System.Windows.Forms.ListBox event_list;
     }
 }
